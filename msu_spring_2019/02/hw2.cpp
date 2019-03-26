feafc6686485482018419688046478153cbe77be
Georgy Bronytsky <getibr@gmail.com>
2019-03-26 18:38:51 +0300

    hw2 v.1

--- msu_spring_2019/02/hw2.cpp
+++ msu_spring_2019/02/hw2.cpp
@@ -11,15 +11,6 @@ public:
         return add_sub(expr);
     }
 private:
-    struct Operations
-    {
-        static const char Add = '+';
-        static const char Sub = '-'; 
-        static const char Mul = '*';
-        static const char Div = '/';
-        static const char Minus = '-';
-    };
-
     int64_t add_sub(std::istringstream &expr)
     {
         int64_t right, left = div_mul(expr);
@@ -27,9 +18,9 @@ private:
         while (expr >> oper)
         {
             right = div_mul(expr);
-            if (oper == Operations::Add)
+            if (oper == '+')
                 left += right;
-            if (oper == Operations::Sub)
+            if (oper == '-')
                 left -= right;
         }
         return left;
@@ -37,14 +28,14 @@ private:
 
     int64_t div_mul(std::istringstream &expr)
     {
-        int64_t right, left = number_s_minusom(expr);
+        int64_t right, left = number(expr);
         char oper;
-        while (expr >> oper && (oper == Operations::Mul || oper == Operations::Div))
+        while (expr >> oper && (oper == '*' || oper == '/'))
         {
             right = div_mul(expr);
-            if (oper == Operations::Mul) 
+            if (oper == '*') 
                 left *= right;
-            if (oper == Operations::Div)
+            if (oper == '/')
             {
                 if (right == 0) 
                     throw std::invalid_argument("");
@@ -56,20 +47,20 @@ private:
         return left;
     }
 
-    int64_t number_s_minusom(std::istringstream &expr)
+    int64_t number(std::istringstream &expr)
     {
         char znak;
-        int64_t znak_opredel = 1;
+        int64_t znak_chisla = 1;
         if (!(expr >> znak))
             throw std::invalid_argument("");
-        if (znak == Operations::Minus)
-            znak_opredel = -1;
+        if (znak == '-')
+            znak_chisla = -1;
         else 
             expr.putback(znak);
         int64_t number = 0;
         if (!(expr >> number))
             throw std::invalid_argument("");
-        return (number * znak_opredel);
+        return (number * znak_chisla);
     }
 };
 
