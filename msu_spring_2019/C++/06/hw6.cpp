f95a3b651f8db38d7a37e81a8138c0be78151c9b
Georgy Bronytsky <getibr@gmail.com>
2019-05-16 14:53:32 +0300

    hw6.v1

--- msu_spring_2019/C++/06/hw6.cpp
+++ msu_spring_2019/C++/06/hw6.cpp
@@ -3,30 +3,31 @@
 #include <fstream>
 #include <thread>
 
-const char* input_file = "input.bin";
-const char* output_file = "output.bin";
+using file_ptr = std::unique_ptr<FILE, int(*)(FILE*)>;
+
+std::string input = "input.bin";
+std::string output = "output.bin";
 
 uint64_t file_size(const std::string &filename)
 {
-    FILE *f = fopen(filename.c_str(),"rb"); 
-    fseek(f,0,SEEK_END);
-    int size = ftell(f);
-    fclose(f);
+    file_ptr f(fopen(filename.c_str(), "rb"), &fclose);
+    fseek(f.get(),0,SEEK_END);
+    int size = ftell(f.get());
     return size;
 }
 
 void mergefile(const std::string &sort_file1, const std::string &sort_file2, const std::string &sorted_file)
 {
-    FILE* out = fopen(sorted_file.c_str(), "wb");
-    FILE* sort1 = fopen(sort_file1.c_str(), "rb");
-    FILE* sort2 = fopen(sort_file2.c_str(), "rb");
+    file_ptr out(fopen(sorted_file.c_str(), "wb"), &fclose);
+    file_ptr sort1(fopen(sort_file1.c_str(), "rb"), &fclose);
+    file_ptr sort2(fopen(sort_file2.c_str(), "rb"), &fclose);
 
     uint64_t sort1_size = file_size(sort_file1) / sizeof(uint64_t);
     uint64_t sort2_size = file_size(sort_file2) / sizeof(uint64_t);
     uint64_t temp1, temp2;
 
-    fread(&temp1, sizeof(temp1), 1, sort1);
-    fread(&temp2, sizeof(temp2), 1, sort2);
+    fread(&temp1, sizeof(temp1), 1, sort1.get());
+    fread(&temp2, sizeof(temp2), 1, sort2.get());
     while(sort1_size != 0 || sort2_size != 0)
     {
         if (sort1_size == 0)
@@ -34,9 +35,9 @@ void mergefile(const std::string &sort_file1, const std::string &sort_file2, con
             while(sort2_size != 0)
             {
                 sort2_size--;
-                fwrite(&temp2, sizeof(temp2), 1, out);
+                fwrite(&temp2, sizeof(temp2), 1, out.get());
                 if(sort2_size > 0)
-                    fread(&temp2, sizeof(temp2), 1, sort2);
+                    fread(&temp2, sizeof(temp2), 1, sort2.get());
             }
         }
         else if (sort2_size == 0)
@@ -44,31 +45,27 @@ void mergefile(const std::string &sort_file1, const std::string &sort_file2, con
             while(sort1_size != 0)
             {
                 sort2_size--;
-                fwrite(&temp1, sizeof(temp1), 1, out);
+                fwrite(&temp1, sizeof(temp1), 1, out.get());
                 if(sort1_size > 0)
-                    fread(&temp1, sizeof(temp1), 1, sort1);
+                    fread(&temp1, sizeof(temp1), 1, sort1.get());
             }
         }
         else if(temp1 <= temp2)
         {
             sort1_size--;
-            fwrite(&temp1, sizeof(temp1), 1, out);
+            fwrite(&temp1, sizeof(temp1), 1, out.get());
             if(sort1_size > 0)
-                fread(&temp1, sizeof(temp1), 1, sort1);
+                fread(&temp1, sizeof(temp1), 1, sort1.get());
         }
         else
         {
             sort2_size--;
-            fwrite(&temp2, sizeof(temp2), 1, out);
+            fwrite(&temp2, sizeof(temp2), 1, out.get());
             if(sort2_size > 0)
-                fread(&temp2, sizeof(temp2), 1, sort2);
+                fread(&temp2, sizeof(temp2), 1, sort2.get());
         }
     }
-    fclose(sort1);
-    fclose(sort2);
-    fclose(out);
 }
-
 
 std::string mergesort_file(uint64_t left, uint64_t right, uint step)
 {
@@ -78,14 +75,15 @@ std::string mergesort_file(uint64_t left, uint64_t right, uint step)
     if (left == right)
     {
         std::string sorted_file = std::string(std::to_string(left) + '-' + std::to_string(right) + '-' + std::to_string(step));
-        FILE* out = fopen(sorted_file.c_str(), "wb");
-        FILE* in = fopen(input_file, "rb");
-        fseek(in, sizeof(uint64_t) * left, SEEK_SET);
+        file_ptr in(fopen(input.c_str(), "rb"), &fclose);
+        file_ptr out(fopen(sorted_file.c_str(), "rb"), &fclose);
+
+        fseek(in.get(), sizeof(uint64_t) * left, SEEK_SET);
         uint64_t temp;
-        fread(&temp, sizeof(temp), 1, in);
-        fwrite(&temp, sizeof(temp), 1, out);
-        fclose(in);
-        fclose(out);
+        fread(&temp, sizeof(temp), 1, in.get());
+        fwrite(&temp, sizeof(temp), 1, out.get());
+        fclose(in.get());
+        fclose(out.get());
         return sorted_file;
     }
     sort_file1 = mergesort_file(left, middle, step + 1);
@@ -97,13 +95,13 @@ std::string mergesort_file(uint64_t left, uint64_t right, uint step)
 
 int main(void)
 {
-    uint64_t count_num = file_size(input_file) / sizeof(uint64_t);
+    uint64_t count_num = file_size(input) / sizeof(uint64_t);
     std::thread file_left(mergesort_file, 0, 0, (count_num - 1) / 2);
     std::thread file_right(mergesort_file, 0, (count_num - 1) / 2 + 1, count_num - 1);
     file_left.join();
     file_right.join();
     std::string str1 = std::string(std::to_string(0) + '-' + std::to_string((count_num - 1) / 2) + '-' + std::to_string(0));
     std::string str2 = std::string(std::to_string((count_num - 1) / 2 + 1) + '-' + std::to_string(count_num - 1) + '-' + std::to_string(0));
-    mergefile(str1, str2, output_file);
+    mergefile(str1, str2, output);
     return 0;
 }
